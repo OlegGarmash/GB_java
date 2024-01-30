8b7ba87040242fc6ef71536b44a76cdcd3b9601c
OlegGarmash <oleg@stavrol.ru>
2024-01-30 13:23:39 +0400

    Семинар 3

---
+++ src/main/java/ru/gb_tasks/s3/task02/Main.java
@@ -0,0 +1,27 @@
+package ru.gb_tasks.s3.task02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        Student student1 = new Student("Petrov", 101, 5000, Arrays.asList(5, 4, 3));
+//        Student student2 = new Student("Petrov", 102, 6000, Arrays.asList(5, 4, 5));
+//        Student student3 = new Student("Petrov", 103, 7000, Arrays.asList(5, 3, 3));
+//        Student student4 = new Student("Petrov", 104, 8000, Arrays.asList(3, 4, 3));
+
+        List<Student> studentList = new ArrayList<>(Arrays.asList(
+                new Student("Petrov", 101, 5000, Arrays.asList(5, 4, 3)),
+                new Student("Smirnov", 102, 6000, Arrays.asList(5, 4, 5)),
+                new Student("Sidorov", 103, 7000, Arrays.asList(5, 3, 3)),
+                new Student("Ivanov", 104, 8000, Arrays.asList(3, 4, 3))
+        ));
+
+        for (Student student : studentList) {
+            if (student.getSurname().endsWith("ov") && student.evenGrades()) {
+                System.out.println("Stipendia = " + student.getStipendia());
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/gb_tasks/s3/task03/Cube.java
@@ -0,0 +1,50 @@
+package ru.gb_tasks.s3.task03;
+
+public class Cube {
+    private Integer lenght;
+    private String color;
+    private String material;
+
+    public Cube(Integer lenght, String color, String material) {
+        this.lenght = lenght;
+        this.color = color;
+        this.material = material;
+    }
+
+    public Integer getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(Integer lenght) {
+        this.lenght = lenght;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    @Override
+    public String toString() {
+        return "Cube{" +
+                "lenght=" + lenght +
+                ", color='" + color + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
+
+    public int getVolume() {
+        return (int) Math.pow(lenght, 3);
+    }
+}
