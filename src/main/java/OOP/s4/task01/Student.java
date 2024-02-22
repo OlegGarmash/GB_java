8cda4c631dc956ab87df5be32e03a4da4b660a42
OlegGarmash <oleg@stavrol.ru>
2024-02-22 12:50:53 +0400

    ООП 4 семинар

---
+++ src/main/java/OOP/s4/task01/Student.java
@@ -0,0 +1,32 @@
+package OOP.s4.task01;
+
+public class Student extends User implements Comparable<Student>{
+    public Long studentId;
+
+    public Student(String firstName, String secondName, String lastName) {
+        super(firstName, secondName, lastName);
+        this.studentId;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.studentId > o.studentId)
+            return 1;
+        else if (this.studentId < o.studentId)
+            return -1;
+        else
+            return 0;
+        //return this.studentId.compareTo(o.studentId);
+    }
+}
